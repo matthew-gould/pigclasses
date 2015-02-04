b7c02fde8fc74d32799e01167a82587edacd4299
matthew gould <gouldm44@gmail.com>
2015-02-04 15:16:10 -0500

    Added more working tests and sides method to game class.

--- game.rb
+++ game.rb
@@ -4,11 +4,25 @@ class Game
 
   def initialize players=0
     @players = players
+    @name = []
   end
 
   def names
-    
+    players.times do |x|
+      puts "\nPlayer#{x+1} what is your name?"
+        y = gets.chomp.downcase.capitalize
+        @name.push y
+    end
+     @name
   end
+
+  def sides
+    puts "How sides would you like to play with?"
+      sides = gets.chomp.to_i
+
+    return sides
+  end
+
 
 
 
--- pigstick_test.rb
+++ pigstick_test.rb
@@ -13,18 +13,26 @@ class PigstickTests < MiniTest::Test
     assert game.players, 2
   end
 
-  def test_that_you_can_enter_numner_of_players
-    puts "How many players?"
-      x = gets.chomp.to_i
-      game = Game.new x
-      assert game.players, x
-  end
+  # TEST ONLY WORKS IF YOU GET AN INPUT FOR # OF PLAYERS:
+
+  # def test_that_you_can_enter_numner_of_players
+  #   puts "How many players?"
+  #     x = gets.chomp.to_i
+  #     game = Game.new x
+  #     assert game.players, x
+  # end
 
   def test_that_players_can_enter_names
     game = Game.new 3
+    assert game.names.count, 3
+  end
 
+  # TEST ONLY WORKS IF YOU CHOOSE 3 SIDIES:
 
-
+  # def test_that_dice_sides_can_be_entered
+  #   game = Game.new 3
+  #   assert game.sides == 3
+  # end
 
 
 
