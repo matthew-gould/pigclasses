bd6bbfd23a3e7f27fe2ab2111eb61afefbf99f30
matthew gould <gouldm44@gmail.com>
2015-02-08 17:04:12 -0500

    almost working pig

--- game.rb
+++ game.rb
@@ -1,35 +1,58 @@
 class Game
 require 'pry'
-  attr_reader :players, :player_total, :turn_score
+  attr_reader :turn_score, :max_score
 
-  def initialize players=1
-    @players = players
-    @name = []
-    @player_totals = Array.new(players, 0)
+  def initialize
+    @get_names = []
+    @score_array = []
     @turn_score = 0
+    max_score = 50
   end
 
-  def names
-    players.times do |x|
-      puts "\nPlayer#{x+1} what is your name?"
-        y = gets.chomp.downcase.capitalize
-        @name.push y
+  def get_names(name)
+    @get_names = name
+    score_array(@get_names)
+    return @get_names
+  end
+
+  def score_array(get_names)
+    @get_names.each do |x|
+      @score_array << 0
+      return @score_array
     end
-     @name
   end
 
   def score
-    (0...@player_totals.length).each do |x|
-      puts "\n#{@name[x]}, you currently have #{@player_totals[x]} points."
-      @player_totals[x] += @turn_score
-      return @player_totals
+    (0..@score_array.length).each do |x|
+      @score_array[x] += @turn_score
+      puts "\n#{@get_names[x]}, you currently have #{@score_array[x]} points.\n\n"
+        if @score_array[x] >= max_score
+          @winner = x
+          #return over?
+        end
+      end
+  end
+
+  def over?
+    if true
+      puts "\n\nWe have a winner!\n\n" 
+      puts "\n\nFinal Scoreboard:\n\n"
+      (0...score_array.length).each do |x|
+        puts "#{player_name[x]}: #{player_scores[x]}"
+      end
+    else
+      return false
     end
   end
 
-  def roll_once
+
+  def rolling
+    roll = rand(1..6)
+    roll_once(roll)
+  end
+
+  def roll_once (roll)
     loop do
-      roll = rand(1..6)
-        puts "Your roll is: #{roll}!\n\n"
         if roll == 1
           puts "DOH! You got 0 points for rolling a 1!\n\n"
           return 0
@@ -43,6 +66,4 @@ require 'pry'
       end
     end
   end
-
-
 end
--- pigstick_game.rb
+++ pigstick_game.rb
@@ -1,4 +1,45 @@
+# require 'simplecov'
+# SimpleCov.start
 require './game'
 require './pig'
 require './hog'
+require 'pry'
 
+# Game start:
+
+puts "\n\n*******************************"
+puts "*****Welcome to HOG ROAST!*****"
+puts "*******************************\n\n"
+
+game = Game.new
+
+name = []
+players = 0
+game_mode = []
+
+puts "\nHow many players?"
+  players = gets.chomp.to_i
+
+players.times do |x|
+      puts "\nPlayer#{x+1} what is your name?"
+        y = gets.chomp.downcase.capitalize
+        name.push y
+        game.get_names(name)
+    end
+
+until game.over?
+  name.each do |x|
+    puts "\nIt is now #{x} turn."
+    game.rolling
+  end
+end
+
+
+
+ # puts "\nWhich game would you like to play?"
+ # puts "1) Pig\n2) Hog"
+ #  game_mode = gets.chomp.downcase
+
+ #  if game_mode = pig
+
+###############################################
